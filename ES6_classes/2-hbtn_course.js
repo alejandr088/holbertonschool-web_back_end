e6eeeadad24654372dd2bf876a0c48c44895d8f0
alejandr088 <alejandrivello@gmail.com>
2023-09-17 18:48:50 -0700

    tasks 0 to 6

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -17,4 +17,35 @@ export default class HolbertonCourse {
     this._students = students;
   }
 
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw TypeError('Length must be a number');
+    }
+    this._length = length;
+  }
+
+  set students(students) {
+    if (!Array.isArray(students)) {
+      throw TypeError('Students must be an array of strings');
+    }
+  }
 }
--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -6,21 +6,21 @@ export default class Currency {
 
     get code() {
         return this._code;
-        }
+    }
 
     get name() {
         return this._name;
-        }
+    }
 
     set code(code) {
         this._code = code;
-        }
+    }
 
     set name(name) {
         this._name = name;
-        }
+    }
 
     displayFullCurrency() {
         return `${this.name} (${this.code})`;
-        }
+    }
 }
