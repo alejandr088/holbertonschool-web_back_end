62f7f79a3ac47ba4aff408c129b9b280cafee617
alejandr088 <alejandrivello@gmail.com>
2023-10-14 10:09:12 -0700

    fixed lint

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,9 +1,12 @@
-import {uploadPhoto, createUser} from './utils.js';
+import { uploadPhoto, createUser } from "./utils.js";
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then((values) => {
-      console.log(values[0].body, values[1].firstName, values[1].lastName);
+  Promise.all([uploadPhoto(), createUser()])
+    .then((results) => {
+      const [photo, user] = results;
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
-    .catch(() => console.log('Signup system offline'));
+    .catch((error) => {
+      console.log("Signup system offline");
+    });
 }
